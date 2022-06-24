2795b96eb4d2a3e2ac29fcba07052beb26257781
Toby Driscoll <driscoll@udel.edu>
2022-06-24 13:17:01 -0400

    Add to function arguments

--- functions.jl
+++ functions.jl
@@ -21,7 +21,7 @@ function roots(a,b,c)
         d = complex(d)
     end
     x₁ = (-b-sign(b)*sqrt(d)) / 2a 
-    return x₁,c/(a*x₁)
+    return x₁,c/(a*x₁)   # return keyword not strictly needed
 end
 
 roots isa Function
@@ -39,3 +39,41 @@ plus1.(t)
 
 log.(1 .+ sin.(t))
 @. log(1+sin(t))
+
+
+## Default argument values
+function foo(x,y=6)
+    return x+y
+end
+foo(2,4)
+foo(2)
+
+vander(x,n=length(x)-1) = [ x^j for x in x, j in 0:n ]
+vander(-1:3)
+vander(-1:3,2)
+
+
+## Slurping
+add_three(args...) = args[1] + args[2] + args[3]
+add_three(1,2,3,4,5,6)
+
+
+## Keyword arguments
+function bar(x;rev)
+    return rev ? reverse(x) : x
+end
+
+bar(1:5)
+bar(1:5,true)
+bar(1:5,rev=true)
+rev = false
+bar(1:5;rev=rev)
+
+function bar(x;rev=true)
+    return rev ? reverse(x) : x   # abbreviated if/then/else
+end
+
+bar(1:5)
+bar(1:5,true)
+rev = false
+bar(1:5;rev)
